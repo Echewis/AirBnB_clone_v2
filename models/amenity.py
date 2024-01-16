76039aefb10f3f6bec04cf7148dbb3a15e4bb7c2
WisDomz <houseofwem1@gmail.com>
2024-01-16 13:37:24 +0100

    models files

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is a state Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage_type
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    This is the amenity class
+    """
+    __tablename__ = 'amenities'
+
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""This is a city Module for HBNB project """
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """This is the city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        state_id = ""
+        name = ""
