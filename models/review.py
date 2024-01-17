ed45069a7ca5f27a2c4f7e8f2e0cf94369b5ae05
WisDomz <houseofwem1@gmail.com>
2024-01-17 13:43:25 +0100

    update

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
+""" This will review module for the HBNB project """
+from models import storage_type
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from models.base_model import BaseModel, Base
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ This will review class to store review information """
     __tablename__ = 'reviews'
     if storage_type == 'db':
         text = Column(String(1024), nullable=False)
